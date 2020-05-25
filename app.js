f5cd54d20e399e855772305e39d76cb94e23982d
Hayoun Lee <sidhlee@gmail.com>
2020-05-25 19:15:26 +0900

    89. Adding place-specific routes

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const placesRoutes = require('./routes/places-routes');
 const app = express();
 
 // use your routes here
-app.use(placesRoutes);
+// If no path is given, all route paths will be "exact"
+app.use('/api/places', placesRoutes); // routes are added at the given path
 
 // start server
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,38 @@ const express = require('express'); // have to require express in every file tha
 
 const router = express.Router(); // run express's router factory to create router
 
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Youido Park',
+    description: 'Beautiful park surrounded by sky scrapers in Youido, Seoul',
+    imageUrl: 'https://placem.at/places?w=1260&h=750&random=1',
+    address: '68 Yeouigongwon-ro, Yeoui-dong, Yeongdeungpo-gu, Seoul', // cspell: disable-line
+    location: {
+      lat: 37.524482,
+      lng: 126.919066,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: ' Park',
+    description: 'Beautiful park surrounded by sky scrapers in Youido, Seoul',
+    imageUrl: 'https://placem.at/places?w=1260&h=750&random=2',
+    address: '68 Yeouigongwon-ro, Yeoui-dong, Yeongdeungpo-gu, Seoul', // cspell: disable-line
+    location: {
+      lat: 37.524482,
+      lng: 126.919066,
+    },
+    creator: 'u2',
+  },
+];
+
 // Add your callbacks at the specified routes & methods
-router.get('/', (req, res, next) => {
-  console.log('GET request in place');
-  res.json({ message: 'works!' });
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  res.json({ place });
 });
 
 // and export with CJS module export
