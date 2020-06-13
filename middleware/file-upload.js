0bdc74d6590b0ebf049698fc7242b59bc32068a7
Hayoun Lee <sidhlee@gmail.com>
2020-06-13 12:21:41 +0900

    164. Filtering files on the backend (images only!)

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -24,6 +24,15 @@ const fileUpload = multer({
       cb(null, uuid() + '.' + ext);
     },
   }),
+  // we shouldn't trust the validation from the client because it is hackable.
+  // Frontend validation is for better user-experience, not data-sanitization.
+  fileFilter: (req, file, cb) => {
+    // If the mimetype exists in the map, convert it to boolean with double bang operator
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    const error = isValid ? null : new Error('Invalid mime type!');
+    // arg1 - error (if any), arg2 - accept:boolean
+    cb(error, isValid);
+  },
 });
 
 module.exports = fileUpload;
