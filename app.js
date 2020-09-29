c66b9ba1c6dac1e491949873d933cef6790c4c52
Hayoun Lee <sidhlee@gmail.com>
2020-09-29 23:39:08 +0900

    Add boilerplates for routers and controllers

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const HttpError = require('./models/http-error');
 
+const usersRouter = require('./routes/users-routes');
+const placesRouter = require('./routes/places-routes');
+
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/api/users', usersRouter);
+app.use('/api/places', placesRouter);
+
 app.use(() => {
   throw new HttpError('Could not find the requested page.', 404);
-})
+});
 
 app.use((err, req, res, next) => {
   console.log(err);
@@ -20,14 +26,21 @@ app.use((err, req, res, next) => {
     return next(err);
   }
   return res.status(err.code || 500).json({
-    message: err.message || 'An unknown error occurred.'
-  })
-})
+    message: err.message || 'An unknown error occurred.',
+  });
+});
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true}, (mongoError) => {
-  if (mongoError) { console.log('Error connecting to mongoDB...'); }
-  else { console.log('connected to db...')}
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`Listening to port: ${process.env.PORT || 5000}`)
-  })
-})
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (mongoError) => {
+    if (mongoError) {
+      console.log('Error connecting to mongoDB...');
+    } else {
+      console.log('connected to db...');
+    }
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Listening to port: ${process.env.PORT || 5000}`);
+    });
+  }
+);
---
+++ routes/places-routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {
+  getPlaceById,
+  getPlacesByUserId,
+  createPlace,
+  updatePlace,
+  deletePlace,
+} = require('../controllers/places-controllers');
+
+const router = express.Router();
+
+router.get('/:pid', getPlaceById);
+router.get('/:uid', getPlacesByUserId);
+router.post('/', createPlace);
+router.patch('/:pid', updatePlace);
+router.delete('/:pid', deletePlace);
+
+module.export = router;
