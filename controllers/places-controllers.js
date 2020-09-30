d997a3d63007f5d0db4b75130b8c274fb6df3d2b
Hayoun Lee <sidhlee@gmail.com>
2020-09-30 21:26:42 +0900

    Add getPlaceById & getPlacesByUserId

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,48 @@
-const getPlaceById = async (req, res, next) => {};
-const getPlacesByUserId = async (req, res, next) => {};
+const HttpError = require('../models/http-error');
+const Place = require('../models/place');
+const User = require('../models/user');
+
+const getPlaceById = async (req, res, next) => {
+  const { pid } = req.params;
+
+  let place;
+  try {
+    place = await Place.findById(pid);
+  } catch (err) {
+    return next(
+      new HttpError('An error occurred while finding the place.', 500)
+    );
+  }
+  if (!place) {
+    return next(new HttpError('Could not find a place for the given id.', 422));
+  }
+
+  return res.json({ place: place.toObject({ getters: true }) });
+};
+
+const getPlacesByUserId = async (req, res, next) => {
+  const { uid } = req.params;
+
+  let user;
+  try {
+    user = await (await User.findById(uid)).populate('places');
+  } catch (err) {
+    return next(
+      new HttpError('An error occurred while finding the user.', 500)
+    );
+  }
+  if (!user) {
+    return next(new HttpError('Could not find a user with the given id.', 422));
+  }
+  if (user.places.length === 0) {
+    return next(new HttpError('Could not find any places for the user.', 404));
+  }
+
+  return res.json({
+    places: user.places.map((p) => p.toObject({ getters: true })),
+  });
+};
+
 const createPlace = async (req, res, next) => {};
 const updatePlace = async (req, res, next) => {};
 const deletePlace = async (req, res, next) => {};
@@ -9,5 +52,5 @@ module.exports = {
   getPlacesByUserId,
   createPlace,
   updatePlace,
-  deletePlace
-}
+  deletePlace,
+};
