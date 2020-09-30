930ca7b05c50eda1de44f15abb5f86afa6daebe2
Hayoun Lee <sidhlee@gmail.com>
2020-09-30 17:39:33 +0900

    Add validation for signup payload

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { check } = require('express-validator');
+
 const { getUsers, signup, login } = require('../controllers/users-controllers');
 
 const router = express.Router();
 
 router.get('/', getUsers);
-router.post('/signup', signup);
+
+router.post(
+  '/signup',
+  // must include validation when pushing into the db
+  [
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 6 }),
+  ],
+  signup
+);
+
 router.post('/login', login);
 
 module.exports = router;
